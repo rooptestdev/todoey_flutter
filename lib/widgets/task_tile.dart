4a88e263806a92abca97362833ab2322e9e4488e
rooptestdev <92123921+rooptestdev@users.noreply.github.com>
2022-03-06 04:20:43 +0530

    local state example

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -9,10 +9,30 @@ class TaskTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       title: Text('This is task'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: null,
-      ),
+      trailing: TaskCheckbox(),
+    );
+  }
+}
+
+class TaskCheckbox extends StatefulWidget {
+  const TaskCheckbox({Key? key}) : super(key: key);
+
+  @override
+  State<TaskCheckbox> createState() => _TaskCheckboxState();
+}
+
+class _TaskCheckboxState extends State<TaskCheckbox> {
+  bool? isChecked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: isChecked,
+      onChanged: (newValue) {
+        setState(() {
+          isChecked = newValue;
+        });
+      },
     );
   }
 }
