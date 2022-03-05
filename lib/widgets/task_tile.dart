b0a3b7fcbc83aa432e2e8b1a6709ef26f234895a
rooptestdev <92123921+rooptestdev@users.noreply.github.com>
2022-03-06 04:37:37 +0530

    global state example

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,37 +1,46 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
   const TaskTile({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
+  @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is task'),
-      trailing: TaskCheckbox(),
+      title: Text(
+        'This is task',
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
+      ),
+      trailing: TaskCheckbox(
+        checkboxState: isChecked,
+      ),
     );
   }
 }
 
-class TaskCheckbox extends StatefulWidget {
-  const TaskCheckbox({Key? key}) : super(key: key);
-
-  @override
-  State<TaskCheckbox> createState() => _TaskCheckboxState();
-}
-
-class _TaskCheckboxState extends State<TaskCheckbox> {
-  bool? isChecked = false;
+class TaskCheckbox extends StatelessWidget {
+  final bool checkboxState;
+  const TaskCheckbox({Key? key, required this.checkboxState});
   @override
   Widget build(BuildContext context) {
     return Checkbox(
       activeColor: Colors.lightBlueAccent,
-      value: isChecked,
+      value: checkboxState,
       onChanged: (newValue) {
-        setState(() {
-          isChecked = newValue;
-        });
+        // setState(() {
+        //   isChecked = newValue;
+        // });
       },
     );
   }
